72013ecb063a8ed11ec69e2d56fe84bdb6f50465
Alejandro Falkowski <alexrfalkowski@gmail.com>
2025-11-08 09:14:27 +0100

    feat(timeout): add handler for starting async (#3)

--- sync.go
+++ sync.go
@@ -8,7 +8,7 @@ import (
 // Handler used for sync.
 type Handler func(context.Context) error
 
-// Wait for the handler to complete or continue.
+// Wait will wait for the handler to complete or continue.
 func Wait(ctx context.Context, timeout time.Duration, handler Handler) error {
 	ch := make(chan error, 1)
 	go func() {
@@ -20,5 +20,23 @@ func Wait(ctx context.Context, timeout time.Duration, handler Handler) error {
 		return err
 	case <-time.After(timeout):
 		return nil
+	}
+}
+
+// Timeout will wait for the handler to complete or timeout.
+func Timeout(ctx context.Context, timeout time.Duration, handler Handler) error {
+	ctx, cancel := context.WithTimeout(ctx, timeout)
+	defer cancel()
+
+	ch := make(chan error, 1)
+	go func() {
+		ch <- handler(ctx)
+	}()
+
+	select {
+	case err := <-ch:
+		return err
+	case <-time.After(timeout):
+		return ctx.Err()
 	}
 }
--- sync_test.go
+++ sync_test.go
@@ -21,8 +21,27 @@ func TestWaitError(t *testing.T) {
 	}))
 }
 
-func TestWaitTimeout(t *testing.T) {
+func TestWaitContinue(t *testing.T) {
 	require.NoError(t, sync.Wait(t.Context(), time.Microsecond, func(context.Context) error {
+		time.Sleep(time.Second)
+		return nil
+	}))
+}
+
+func TestTimeoutNoError(t *testing.T) {
+	require.NoError(t, sync.Timeout(t.Context(), time.Second, func(context.Context) error {
+		return nil
+	}))
+}
+
+func TestTimeoutError(t *testing.T) {
+	require.Error(t, sync.Timeout(t.Context(), time.Second, func(context.Context) error {
+		return context.Canceled
+	}))
+}
+
+func TestTimeout(t *testing.T) {
+	require.ErrorIs(t, context.DeadlineExceeded, sync.Timeout(t.Context(), time.Microsecond, func(context.Context) error {
 		time.Sleep(time.Second)
 		return nil
 	}))
