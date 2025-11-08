02e18e5f71bbcfb57bbfdfb788886a1a9439e67e
Alejandro Falkowski <alexrfalkowski@gmail.com>
2025-11-08 08:56:13 +0100

    feat(wait): add handler for starting async (#2)

---
+++ sync_test.go
@@ -0,0 +1,29 @@
+package sync_test
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/alexfalkowski/go-sync"
+	"github.com/stretchr/testify/require"
+)
+
+func TestWaitNoError(t *testing.T) {
+	require.NoError(t, sync.Wait(t.Context(), time.Second, func(context.Context) error {
+		return nil
+	}))
+}
+
+func TestWaitError(t *testing.T) {
+	require.Error(t, sync.Wait(t.Context(), time.Second, func(context.Context) error {
+		return context.Canceled
+	}))
+}
+
+func TestWaitTimeout(t *testing.T) {
+	require.NoError(t, sync.Wait(t.Context(), time.Microsecond, func(context.Context) error {
+		time.Sleep(time.Second)
+		return nil
+	}))
+}
