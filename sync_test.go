63d36756be94355cb67244ed1f626b99ce852d1a
Alejandro Falkowski <alexrfalkowski@gmail.com>
2025-11-08 10:04:02 +0100

    feat(pool): add a generic sync.Pool (#4)

--- sync_test.go
+++ sync_test.go
@@ -1,6 +1,7 @@
 package sync_test
 
 import (
+	"bytes"
 	"context"
 	"testing"
 	"time"
@@ -8,6 +9,27 @@ import (
 	"github.com/alexfalkowski/go-sync"
 	"github.com/stretchr/testify/require"
 )
+
+// NewBufferPool for sync.
+func NewBufferPool() *BufferPool {
+	return &BufferPool{pool: sync.NewPool[bytes.Buffer]()}
+}
+
+// BufferPool for sync.
+type BufferPool struct {
+	pool *sync.Pool[bytes.Buffer]
+}
+
+// Get a new buffer.
+func (p *BufferPool) Get() *bytes.Buffer {
+	return p.pool.Get()
+}
+
+// Put the buffer back.
+func (p *BufferPool) Put(buffer *bytes.Buffer) {
+	buffer.Reset()
+	p.pool.Put(buffer)
+}
 
 func TestWaitNoError(t *testing.T) {
 	require.NoError(t, sync.Wait(t.Context(), time.Second, func(context.Context) error {
@@ -45,4 +67,23 @@ func TestTimeout(t *testing.T) {
 		time.Sleep(time.Second)
 		return nil
 	}))
+}
+
+func TestPool(t *testing.T) {
+	pool := NewBufferPool()
+	buffer := pool.Get()
+	defer pool.Put(buffer)
+
+	require.NotNil(t, buffer)
+}
+
+func BenchmarkPool(b *testing.B) {
+	pool := NewBufferPool()
+
+	b.Run("Get", func(b *testing.B) {
+		for b.Loop() {
+			buffer := pool.Get()
+			pool.Put(buffer)
+		}
+	})
 }
