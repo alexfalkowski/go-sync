f1df5305f19bdfc28710df919f4a7f404ee58341
Alejandro Falkowski <alexrfalkowski@gmail.com>
2026-02-16 11:02:00 +0100

    feat(singleflight): add group (#100)

---
+++ group_test.go
@@ -0,0 +1,29 @@
+package sync_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/alexfalkowski/go-sync"
+	"github.com/stretchr/testify/require"
+)
+
+func TestGroup(t *testing.T) {
+	g := sync.NewGroup[string]()
+
+	v, err, shared := g.Do("test1", func() (string, error) {
+		return "yes", nil
+	})
+	require.NoError(t, err)
+	require.Equal(t, "yes", v)
+	require.False(t, shared)
+	g.Forget("test1")
+
+	v, err, shared = g.Do("test2", func() (string, error) {
+		return "", context.Canceled
+	})
+	require.Error(t, err)
+	require.Empty(t, v)
+	require.False(t, shared)
+	g.Forget("test2")
+}
